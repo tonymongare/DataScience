f941b4630484af3d8e02c137965c59810731bc52
IsaacTonyloi <tonyloimongare10@students.ku.ac.ke>
2021-07-07 16:41:07 -0700

    Some funy stuff

---
+++ exercise.js
@@ -0,0 +1,15 @@
+
+
+<html>
+<head>
+<title>
+<title>
+</head>
+
+<body>
+<p id='tonyloi'> This is Africa</p>
+
+<button type='button' onclick = documnet.getElementById("tonyloi").innerHTML(I'm not Giving up)>Click Here!</button>
+
+</body>
+</html>
